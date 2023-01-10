073b4e2ac04d76f7c0797936d1eabeb095ce3a36
i.plotnikov <ploBcvxw3>
2023-01-11 03:56:57 +0400

    feat: Добавил сервис для API и константу с URL

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+export const ROUTE_API_URL =
+  process.env.API_URL || 'http://router.project-osrm.org/route/v1/driving/';
---
+++ src/services/api/index.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { ROUTE_API_URL } from '../../constants';
+import { ICoordinates } from '../../types';
+
+export default class ApiService {
+  static async fetchPolyline(startPoint: ICoordinates, endPoint: ICoordinates) {
+    try {
+      return await axios.get(
+        `${ROUTE_API_URL}${startPoint.latitude},${startPoint.longitude};${endPoint.latitude},${endPoint.longitude}?geometries=geojson`,
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
