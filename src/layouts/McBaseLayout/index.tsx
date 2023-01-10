331c5f573f6ce56902198e71681528197568ec06
i.plotnikov <ploBcvxw3>
2023-01-10 04:24:43 +0400

    feat: Добавил лейаут

---
+++ src/layouts/McBaseLayout/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.less';
+
+import { Layout } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+
+interface IMcBaseLayoutProps {
+  children?: React.ReactNode;
+}
+
+const McBaseLayout: React.FC<IMcBaseLayoutProps> = ({ children }) => {
+  return (
+    <Layout className="base-layout">
+      <Content className="base-layout__content">{children}</Content>
+    </Layout>
+  );
+};
+
+export default McBaseLayout;
