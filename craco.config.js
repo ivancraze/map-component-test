34f885ba2f9443e7fdd4398dd6a51e6f31440610
i.plotnikov <ploBcvxw3>
2023-01-11 23:48:35 +0400

    feat: test deploy

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,12 @@
 const CracoLessPlugin = require("craco-less");
+const path = require('path');
 
 module.exports = {
     plugins: [{ plugin: CracoLessPlugin }],
-};
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            paths.appBuild = webpackConfig.output.path = path.resolve('docs');
+            return webpackConfig;
+        }
+    }
+}
