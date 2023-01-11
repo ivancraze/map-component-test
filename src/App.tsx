4b6e9cba9892868463f73ba5a4be4883e304aa95
i.plotnikov <ploBcvxw3>
2023-01-11 04:08:35 +0400

    feat: Добавил вывод компонентов в браузер

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+
 import './App.css';
+
 import McBaseLayout from './layouts/McBaseLayout';
+import McTable from './components/McTable';
+import McMap from './components/McMap';
 
 function App() {
-  return <McBaseLayout>sdaf</McBaseLayout>;
+  return (
+    <McBaseLayout>
+      <McTable />
+      <McMap />
+    </McBaseLayout>
+  );
 }
 
 export default App;
