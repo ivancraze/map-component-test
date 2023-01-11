a9ef53b37ad082825a611094c3a89c3089f73f72
i.plotnikov <ploBcvxw3>
2023-01-11 20:34:04 +0400

    feat: Перенес настройки из компонента карты

--- src/components/McMap/components/RouteMachine/index.tsx
+++ src/components/McMap/components/RouteMachine/index.tsx
@@ -14,6 +14,11 @@ export const RoutingMachine: React.FC = () => {
     (state: RootState) => state.main.selectedClaim,
   );
   const map = useMap();
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  });
 
   useEffect(() => {
     if (!waypoints) return;
