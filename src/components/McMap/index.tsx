d6b83761fdb76e340183047d12ad9cbc00632523
i.plotnikov <ploBcvxw3>
2023-01-11 20:33:46 +0400

    feat: Доработал компонет карты, убрал настройки в роутинг к остальным

--- src/components/McMap/index.tsx
+++ src/components/McMap/index.tsx
@@ -1,30 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { MapContainer, TileLayer } from 'react-leaflet';
-import L, { LatLngExpression } from 'leaflet';
 
 import './index.less';
 import 'leaflet/dist/leaflet.css';
 
-import RoutineMachine from './components/RouteMachine';
-import { RootState } from '../../store';
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+import RoutingMachine from './components/RouteMachine';
 
 const McMap: React.FC = () => {
-  const route = useSelector<RootState, LatLngExpression[]>(
-    (state: RootState) => state.main.selectedClaimPolyline,
-  );
-  console.log(route);
   return (
     <div className="map">
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
+        center={[59.984981, 30.345867]}
+        zoom={11}
         scrollWheelZoom={false}
         style={{ height: '100vh', width: '100wh' }}
       >
@@ -32,7 +19,7 @@ const McMap: React.FC = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <RoutineMachine />
+        <RoutingMachine />
       </MapContainer>
     </div>
   );
